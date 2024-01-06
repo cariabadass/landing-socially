96b9b4cafe629da08e381a538db61c6ad1b8b364
cariabadass <154686124+cariabadass@users.noreply.github.com>
2024-01-06 19:25:09 +0100

    Add files via upload

---
+++ landing socially/js/main.js
@@ -0,0 +1,7 @@
+// Toggle Menu
+function menuToggle(){
+	const menuToggle = document.getElementById('menuToggle');
+	const navigation = document.getElementById('navigation');
+	menuToggle.classList.toggle('active');
+	navigation.classList.toggle('active');
+};
